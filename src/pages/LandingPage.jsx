fe16e06ed5369e0d4b20db48357ec08a7c91249e
borebrian2021 <bkimutai2021@gmail.com>
2022-11-16 23:20:25 +0300

    Added home page image

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const LandingPage = () => {
   return (
     <>
-      <div className="font-jost font-bold text-3xl">LandingPage</div>
+      {/* <div className="font-jost font-bold text-3xl">LandingPage</div>
       <div className="font-dancing">LandingPage</div>
-      <div className="font-cormorant">LandingPage</div>
+      <div className="font-cormorant">LandingPage</div> */}
+      <img src="https://mdbootstrap.com/img/new/slides/041.jpg" class="max-w-full h-auto" alt="..." />
     </>
   );
 };
