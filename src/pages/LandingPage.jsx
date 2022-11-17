2ef45bcf5d83450decd16f25a03af9be89f58e3f
borebrian2021 <bkimutai2021@gmail.com>
2022-11-17 07:42:59 +0300

    Added the second div section.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,17 +2,24 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    
-    
+
+    <div>
 
       <div class="container mx-auto h-screen bg-[url('https://mdbootstrap.com/img/new/slides/041.jpg')] bg-cover bg-center max-h-[573px]  justify-center text-center flex items-center home-background">
-      <p className="font-dancing  text-white text-6xl  ">Cedar Gardens</p>
+        <p className="font-dancing  text-white text-6xl  ">Cedar Gardens</p>
       </div>
+      <div class="container mx-auto">
+  <div class="grid grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-2  xs:grid-cols-1">
+    <div className="w-[100%] bg-slate-500">1</div>
+    <div className="w-[100%] bg-gray-900">2</div>
    
+  </div>
+</div>
+    </div>
   );
 };
 
 export default LandingPage;
- {/* <div className="font-jost font-bold text-3xl">LandingPage</div>
+{/* <div className="font-jost font-bold text-3xl">LandingPage</div>
       <div className="font-dancing">LandingPage</div>
       <div className="font-cormorant">LandingPage</div> */}
