d437a1f4d3dfc7a4df76e412145c4fac27cb0fec
denisdelaki <denisdelaki@gmail.com>
2022-11-17 19:59:04 +0300

    created the tab component for the gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Gallery = () => {
+    const [toggletab, setToggleTab]=useState(1)
+    const handleTab=(index)=>{
+        setToggleTab(index)
+    }
   return (
-    <div>Gallery</div>
+    <div>
+ <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
+    <ul className="flex flex-wrap -mb-px text-sm font-medium text-center"  role="tablist">
+        <li className="mr-2">
+            <button className="inline-block p-4 rounded-t-lg border-b-2" onClick={()=>handleTab(1)}   type="button" role="tab" aria-selected="false">GARDEN</button>
+        </li>
+        <li className="mr-2">
+            <button className="inline-block p-4 rounded-t-lg border-b-2" onClick={()=>handleTab(2)}   type="button" role="tab" aria-selected="false">SWIMMING POOL</button>
+        </li>
+        <li className="mr-2">
+            <button className="inline-block p-4 rounded-t-lg border-b-2" onClick={()=>handleTab(3)}  type="button" role="tab" aria-selected="false">PLAY AREA</button>
+        </li>
+        <li className="mr-2">
+            <button className="inline-block p-4 rounded-t-lg border-b-2"  onClick={()=>handleTab(4)} type="button" role="tab" aria-selected="false">ROOMS</button>
+        </li>
+    </ul>
+    </div>
+     <div>
+
+     </div>
+     {/* create a div to hold the tabs content */}
+     <div className=''>
+    <div className={toggletab===1 ? 'block': 'hidden'}>
+    <div >
+
+    </div>
+        
+    </div>
+     <div className={toggletab===2 ? 'block': 'hidden'}>SWIMMING POOL  images</div>
+     <div className={toggletab===3 ? 'block': 'hidden'}>PLAY AREA images</div>
+     <div className={toggletab===4 ? 'block': 'hidden'}>ROOMS images</div>
+     </div>
+    </div>
   )
 }
 
