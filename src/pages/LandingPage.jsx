81e736882369affe66fc33a34c0f93c8cc31560a
borebrian2021 <bkimutai2021@gmail.com>
2022-11-17 07:15:08 +0300

    Added image and text home page.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 const LandingPage = () => {
   return (
-    <>
-      {/* <div className="font-jost font-bold text-3xl">LandingPage</div>
-      <div className="font-dancing">LandingPage</div>
-      <div className="font-cormorant">LandingPage</div> */}
-      <img src="https://mdbootstrap.com/img/new/slides/041.jpg" class="max-w-full h-auto" alt="..." />
-    </>
+    
+    
+
+      <div class="container mx-auto h-screen bg-[url('https://mdbootstrap.com/img/new/slides/041.jpg')] bg-cover bg-center max-h-[573px]  justify-center text-center flex items-center home-background">
+      <p className="font-dancing  text-white text-6xl  ">Cedar Gardens</p>
+      </div>
+   
   );
 };
 
 export default LandingPage;
+ {/* <div className="font-jost font-bold text-3xl">LandingPage</div>
+      <div className="font-dancing">LandingPage</div>
+      <div className="font-cormorant">LandingPage</div> */}
