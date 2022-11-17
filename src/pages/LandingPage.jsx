caa5ab082aa98053c4a07b8179fadc5e9544cbf2
borebrian2021 <bkimutai2021@gmail.com>
2022-11-17 08:09:25 +0300

    Done with the text section next to the image.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -13,7 +13,17 @@ const LandingPage = () => {
           <div className="w-[100%] text-center justify-center items-center">
             <p className="font-dancing text-2xl font-bold mt-3 align-center">Welcome</p>
             <h4 className="font-cormorant uppercase font-bold text-3xl m-8">Your confort is our concern</h4>
-
+            <div class="grid mt-8 grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-2  xs:grid-cols-1">
+              <div className="w-[100%]">
+              <img class="object-cover h-48 w-96 " src="https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135255/Cedar%20Hotel/Edited/IMG_2741_gpgowb.jpg"/>
+              </div>
+              <div className="p-3 text-left">
+<p>Cedar is the place where you can make memories. We offer the following services: Restaurant, Swimming, Bar, Kids Play Area, Meetings and Accomodation.
+</p><br/><p>
+The hotel is located on Kianjai meru, approximately 17km from Meru town.
+</p>
+              </div>
+            </div>
           </div>
           <div className="w-[100%] bg-gray-900">2</div>
 
