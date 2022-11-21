8953976baa12f9b0f675639cccc77ed62f67e5e8
denisdelaki <denisdelaki@gmail.com>
2022-11-21 16:17:18 +0300

    added animmation to the images in the services

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -32,30 +32,30 @@ const Gallery = () => {
     <div className='sm:grid sm:grid-cols-1 md:grid md:grid-cols-2 md:gap-6 lg:grid lg:grid-cols-2 lg:gap-8'>
         <div className='grid grid-cols-2 gap-4  '>
             <div className='h-3/4'>
-                <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135112/Cedar%20Hotel/Edited/IMG_7158_kxazn0.jpg' alt='garden'/>
+                <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135112/Cedar%20Hotel/Edited/IMG_7158_kxazn0.jpg' alt='garden'/>
             </div>
             <div className='h-3/4'>
-                <img className='h-full w-full'src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135337/Cedar%20Hotel/Edited/IMG_2827_hxfhio.jpg' alt='Garden' />
+                <img className='h-full w-full transform transition-all hover:scale-110'src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135337/Cedar%20Hotel/Edited/IMG_2827_hxfhio.jpg' alt='Garden' />
             </div>
             <div className='col-span-2 h-3/4 w-full -my-14'>
-                <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135357/Cedar%20Hotel/Edited/IMG_7119_rlfhmj.jpg' alt='garden images'/>
+                <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135357/Cedar%20Hotel/Edited/IMG_7119_rlfhmj.jpg' alt='garden images'/>
             </div>
         </div>
         <div className='h-3/4'>
-        <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135118/Cedar%20Hotel/Edited/IMG_7139_rsfj6t.jpg' alt='garden ' /> 
+        <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135118/Cedar%20Hotel/Edited/IMG_7139_rsfj6t.jpg' alt='garden ' /> 
         </div>
         <div className='h-full -my-36'>
-            <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135075/Cedar%20Hotel/Edited/IMG_7140_vojgco.jpg' alt='garden images' />
+            <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135075/Cedar%20Hotel/Edited/IMG_7140_vojgco.jpg' alt='garden images' />
         </div>
         <div className='grid grid-cols-2 gap-2 -my-36 '>
           <div className='h-1/2 w-full'>
-             <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135354/Cedar%20Hotel/Edited/IMG_7099_fph3oo.jpg' alt='indiginous trees'/>
+             <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135354/Cedar%20Hotel/Edited/IMG_7099_fph3oo.jpg' alt='indiginous trees'/>
           </div>
           <div className='h-1/2 w-full'>
-            <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135357/Cedar%20Hotel/Edited/IMG_7119_rlfhmj.jpg' alt='beautiful terrain'/>
+            <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135357/Cedar%20Hotel/Edited/IMG_7119_rlfhmj.jpg' alt='beautiful terrain'/>
           </div>
           <div className='col-span-2 h-full w-full -my-48 '>
-            <img className='h-3/4 w-full ' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135200/Cedar%20Hotel/Edited/IMG_2726_cxcoti.jpg' alt='gardens'/>
+            <img className='h-3/4 w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135200/Cedar%20Hotel/Edited/IMG_2726_cxcoti.jpg' alt='gardens'/>
           </div>
         </div>
     </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
     <div className=''>
       <div className='sm:grid sm:grid-cols-1 md:grid md:grid-cols-2 md:gap-6 lg:grid lg:grid-cols-3 lg:gap-8'>
             <div className='sm:h-4/6  sm:w-full  md:h-3/4  lg:h-3/4 w-full' >
-              <img  className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135321/Cedar%20Hotel/Edited/IMG_2756_vo2tbu.jpg' alt='Bar and restaurant' />
+              <img  className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135321/Cedar%20Hotel/Edited/IMG_2756_vo2tbu.jpg' alt='Bar and restaurant' />
               <div className=' flex justify-end -my-20 '>
                 <div className=' bg-slate-100 sm:h-80 sm:w-full md:w-full md:h-24 lg:w-60 lg:h-24 opacity-70 '>
                   <h1 className='font-bold'>BAR AND RESTAURANT</h1>
@@ -14,7 +14,7 @@ const Services = () => {
               </div>
             </div>
         <div className='sm:h-4/6  sm:w-full  md:h-3/4 lg:h-3/4 w-full'>
-            <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135261/Cedar%20Hotel/Edited/IMG_2718_davs8g.jpg' alt='Party and Events' />
+            <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135261/Cedar%20Hotel/Edited/IMG_2718_davs8g.jpg' alt='Party and Events' />
             <div className=' flex justify-end -my-20 '>
                 <div className=' bg-slate-100 sm:h-80 sm:w-full md:w-full md:h-24 lg:w-60 lg:h-24 opacity-70 '>
                   <h1 className='font-bold'>PARTY AND EVENTS</h1>
@@ -23,7 +23,7 @@ const Services = () => {
             </div>  
         </div>
         <div className='sm:h-4/6  sm:w-full  md:h-3/4 lg:h-3/4 w-full'>
-            <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135111/Cedar%20Hotel/Edited/IMG_2690_rlmeer.jpg' alt='Swimmming ' />
+            <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135111/Cedar%20Hotel/Edited/IMG_2690_rlmeer.jpg' alt='Swimmming ' />
             <div className=' flex justify-end -my-20 '>
               <div className=' bg-slate-100 sm:h-80 sm:w-full md:w-full md:h-24 lg:w-60 lg:h-24 opacity-70 '>
                   <h1 className='font-bold'>SWIMMING</h1>
@@ -32,7 +32,7 @@ const Services = () => {
             </div>   
         </div>
         <div className='sm:h-4/6  sm:w-full  md:h-3/4 lg:h-3/4 w-full -my-6'>
-        <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135111/Cedar%20Hotel/Edited/IMG_7154_yy55dw.jpg' alt='Kids play Area' />
+        <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135111/Cedar%20Hotel/Edited/IMG_7154_yy55dw.jpg' alt='Kids play Area' />
         <div className=' flex justify-end -my-20 '>
             <div className=' bg-slate-100 sm:h-80 sm:w-full md:w-full md:h-24 lg:w-60 lg:h-24 opacity-70 '>
               <h1 className='font-bold'>KIDS PLAY AREA</h1>
@@ -41,7 +41,7 @@ const Services = () => {
         </div> 
         </div>
         <div className='sm:h-4/6  sm:w-full  md:h-3/4 lg:h-3/4 w-full -my-6'>
-        <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135258/Cedar%20Hotel/Edited/IMG_2723_ffgq32.jpg' alt='Meetings' />
+        <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135258/Cedar%20Hotel/Edited/IMG_2723_ffgq32.jpg' alt='Meetings' />
         <div className=' flex justify-end -my-20 '>
             <div className=' bg-slate-100 sm:h-80 sm:w-full md:w-full md:h-24 lg:w-60 lg:h-24 opacity-70 '>
               <h1 className='font-bold'>MEETINGS</h1>
@@ -50,7 +50,7 @@ const Services = () => {
         </div>     
         </div>
         <div className='sm:h-4/6  sm:w-full  md:h-3/4 lg:h-3/4 w-full -my-6'>
-        <img className='h-full w-full' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135100/Cedar%20Hotel/Edited/IMG_7149_gyst5m.jpg' alt='Accomodation' />
+        <img className='h-full w-full transform transition-all hover:scale-110' src='https://res.cloudinary.com/dqab6gg7d/image/upload/v1668135100/Cedar%20Hotel/Edited/IMG_7149_gyst5m.jpg' alt='Accomodation' />
         <div className=' flex justify-end -my-20 '>
             <div className=' bg-slate-100 sm:h-80 sm:w-full md:w-full md:h-24 lg:w-60 lg:h-24  opacity-70 '>
               <h1 className='font-bold'>ACCOMODATION</h1>
