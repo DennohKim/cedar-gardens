1e792b4c5775717dd635599799a4f1e50193dbc2
denisdelaki <denisdelaki@gmail.com>
2022-11-22 14:31:55 +0300

    created the Bar and restaurant page

---
+++ src/pages/BarAndRestaurant.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const BarAndRestaurant = () => {
+  return (
+    <div>
+      <header>
+          <p>Menu That Fits You Palatte</p>
+          <i>Spoon icon</i>
+          <h2>TODAY"S SPECIAL</h2>
+      </header>
+      <div>
+          <div>
+              <h3>Food</h3>
+              <ul>
+                  <li>Kienyeji Chicken</li>
+                  <li>Beef</li>
+                  <li>Pork</li>
+                  <li>Goat</li>
+              </ul>
+              <h3>Accompaniments</h3>
+              <ul>                
+                  <li>Ugali</li>
+                  <li>Rice</li>
+                  <li>Chapati</li>
+                  <li>Chips</li>
+                  <li>Saute</li>
+                  <li>Roast Potatoes</li>
+              </ul>
+          </div>
+          <div>
+            <img src='' alt='' />
+          </div>
+          <div>
+              <ul>
+                  <li>Juices</li>
+                  <li>Soda</li>
+                  <li>Alcohol</li>
+              </ul>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default BarAndRestaurant
