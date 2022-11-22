e94f1e73358126b1903465b4dcc5349084d56a78
borebrian2021 <bkimutai2021@gmail.com>
2022-11-22 21:06:00 +0300

    Added top section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div>
+      <div class=" w-[100%] h-screen bg-[url('https://mdbootstrap.com/img/new/slides/041.jpg')] bg-cover bg-center max-h-[300px]  justify-center text-center flex items-center home-background">
+        <p className="font-jost space tracking-widest  text-white text-2xl font-bold uppercase space">About us</p>
+      </div>
+
+
+    </div>
   )
 }
 
