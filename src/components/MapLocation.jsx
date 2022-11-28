aaa7883edbcbdb477f2530f2d67fdb9c34e38fba
“dennohkim” <kimathidennis38@gmail.com>
2022-11-28 19:37:56 +0300

    fix: map error

--- src/components/MapLocation.jsx
+++ src/components/MapLocation.jsx
@@ -7,10 +7,11 @@ function MapLocation() {
       google = {this.props.google}
       style={{width:"100", height:"100"}}
       zoom={10}
-      intialCenter={
+      intialCenter={{
         lat: -1.2972432500000002,
         lng: 36.785859480261195
-      }
+
+      }}
     
     />
   )
