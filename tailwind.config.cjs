40bb30e20737df93f35bdccb37caa794ef9d9e23
“dennohkim” <kimathidennis38@gmail.com>
2022-11-28 18:59:32 +0300

    fix: fix conflict

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -30,4 +30,37 @@ module.exports = {
      }
   },
   plugins: [],
+}/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+   
+    },
+    fontFamily: {
+      'jost': ['Jost','sans-serif'],
+      'dancing': ['Dancing Script', 'cursive'],
+      'cormorant': ['Cormorant Garamond','serif'],
+    },
+    colors: {
+      'primary-color': '#287216',
+      'button-gray': '#D9D9D9',
+      'dark': '#222222',
+      'white': '#fff',
+      
+      
+    },
+     screens: {
+     'sm': '500px',
+     'md': '768px',
+     'lg': '1024px',
+     'xl': '1280px'
+     }
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
