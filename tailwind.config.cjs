5ab813e8c499da24af2e935bf4fa334db8e4496b
borebrian2021 <bkimutai2021@gmail.com>
2022-11-28 10:13:44 +0300

    Configured flowbite tabs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
@@ -29,5 +30,7 @@ module.exports = {
      'xl': '1280px'
      }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
