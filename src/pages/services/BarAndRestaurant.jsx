773499886413a72e50cd6b5fea20857206727fc4
borebrian2021 <bkimutai2021@gmail.com>
2022-11-28 12:52:49 +0300

    Done with restaurant

--- src/pages/services/BarAndRestaurant.jsx
+++ src/pages/services/BarAndRestaurant.jsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import MainBG from "../../components/MainBG";
 import { motion, AnimatePresence } from "framer-motion";
 import Navbar from "../../components/Navbar";
+import { ThreeDots } from 'react-loader-spinner'
 import Footer from "../../components/Footer";
 const BarAndRestaurant = () => {
   const [toggletab, setToggleTab] = useState(1);
+  const [activeItem, setActiveItem] = useState(0);
+  const [url,handleUrl]=useState("https://res.cloudinary.com/dqab6gg7d/image/upload/v1669350725/Cedar%20Hotel/Edited/chicken_xjwzxk.jpg");
   const handleTab = (index) => {
     setToggleTab(index);
   };
@@ -47,7 +50,6 @@ const BarAndRestaurant = () => {
   const [acompanments, setAcompanments] = useState([
     {
       name: "Ugali",
-      url: "",
       url: "https://res.cloudinary.com/dqab6gg7d/image/upload/v1669350313/Cedar%20Hotel/Edited/ugali_wkgzye.jpg",
     },
     {
@@ -72,7 +74,7 @@ const BarAndRestaurant = () => {
     },
   ]);
 
-  const [viewer, setViewer] = useState("");
+  const [viewer, setViewer] = useState("https://res.cloudinary.com/dqab6gg7d/image/upload/v1669350725/Cedar%20Hotel/Edited/chicken_xjwzxk.jpg");
   const [animate, setAnimate] = useState(true);
 
   const fireAnimation = (url) => {
@@ -92,6 +94,7 @@ const BarAndRestaurant = () => {
           Todays Special
         </p>
         <p className="font-dancing text-3xl">Menu that fits you palatte</p>
+      
       </div>
       <div className="w-[100%] p-auto grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 grid-cols-1 gap-2">
         <div className="w-[100%] ">
@@ -105,7 +108,12 @@ const BarAndRestaurant = () => {
             viewport={{ once: true }}
             class="dark:border-gray-700 ml-[10%]"
           >
-            <ul class="flex -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+            
+
+
+
+
+            <ul class="flex -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400 border-b-2 border-gray-200" >
               <li class="mr-2">
                 <a
                   id="tabs-contact-tab"
@@ -114,10 +122,12 @@ const BarAndRestaurant = () => {
                   role="tab"
                   aria-controls="tabs-contact"
                   aria-selected="false"
-                  class="inline-flex p-4 uppercase font-jost rounded-t-lg border-b-2 text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group focus:border-green-700
-                  active"
+                  className={toggletab === 1 ? "inline-flex p-4 uppercase font-jost rounded-t-lg  text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group text-[15px] transition-all ease-in-out delay-150 focus:border-green-700 border-b-2 border-green-700" : "inline-flex p-4 uppercase font-jost rounded-t-lg border-b-2 text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group  focus:border-green-700"}
+                  onClick={()=>{
+                    setToggleTab(1)
+                  }}
                 >
-                  Vision
+                  Food
                 </a>
               </li>
               <li class="mr-2">
@@ -128,9 +138,12 @@ const BarAndRestaurant = () => {
                   role="tab"
                   aria-controls="tabs-contact"
                   aria-selected="false"
-                  class="inline-flex p-4 uppercase font-jost rounded-t-lg border-b-2 text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group  focus:border-green-700"
+                  className={toggletab === 2 ? "inline-flex p-4 uppercase font-jost rounded-t-lg  text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group text-[15px] transition-all ease-in-out delay-150 focus:border-green-700 border-b-2 border-green-700" : "inline-flex p-4 uppercase font-jost rounded-t-lg border-b-2 text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group  focus:border-green-700"}
+                  onClick={()=>{
+                    setToggleTab(2)
+                  }}
                 >
-                  Mission
+                  Acompaniments
                 </a>
               </li>
               
@@ -142,7 +155,10 @@ const BarAndRestaurant = () => {
                   role="tab"
                   aria-controls="tabs-contact"
                   aria-selected="false"
-                  class="inline-flex p-4 uppercase font-jost rounded-t-lg border-b-2 text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group  focus:border-green-700"
+                  className={toggletab === 3 ? "inline-flex p-4 uppercase font-jost rounded-t-lg  text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group  focus:border-green-700 border-b-2 border-green-700 text-[15px] transition-all ease-in-out delay-150" : "inline-flex p-4 uppercase font-jost rounded-t-lg border-b-2 text-black font-bold border-transparent hover:text-green-700 hover:border-green-700 dark:hover:text-green-700 group  focus:border-green-700"}
+                  onClick={()=>{
+                    setToggleTab(3)
+                  }}
                 >
                   Cocktails
                 </a>
@@ -157,8 +173,7 @@ const BarAndRestaurant = () => {
             transition={{ duration: 1.2 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
-            className="tab-content p-4 lg:ml-[10%] md:ml-[10%] sm:ml-[5%] ml-[10%]"
-            id="tabs-tabContent"
+            id="tabs-tabContent" className={toggletab === 1 ? "block tab-content p-4 lg:ml-[10%] md:ml-[10%] sm:ml-[5%] ml-[10%]" : "hidden" }
           >
             <div
               class="tab-pane  show active"
@@ -169,13 +184,15 @@ const BarAndRestaurant = () => {
               <div className="w-[100%] ">
                 <div className=" w-[100%] lg:ml-[1%] sm:ml-[10%] md:ml-[10%] ml-[5%]">
                   <ul>
-                    {food.map((item) => {
+                    {food.map((item,index) => {
                       return (
                         <li
                           onClick={() => {
                             fireAnimation(item.url);
+                            setActiveItem(index)
                           }}
-                          className="my-5 font-cormorant font-bold flex cursor-pointer hover:text-3xl transition-all ease-in duration-75"
+                          className={activeItem === index? "my-5 font-cormorant font-bold flex cursor-pointer hover:text-3xl transition-all ease-in duration-75 adafasfafaf p-2 bg-primary-color rounded-full text-white":"my-5 font-cormorant font-bold flex cursor-pointer hover:text-3xl transition-all ease-in duration-75"}
+                         
                           key={item.name}
                         >
                           <img
@@ -197,9 +214,109 @@ const BarAndRestaurant = () => {
               aria-labelledby="tabs-profile-tab"
             ></div>
           </motion.div>
+
+          <motion.div
+            initial={{
+              y: -100,
+              opacity: 0,
+            }}
+            transition={{ duration: 1.2 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            id="tabs-tabContent" className={toggletab === 2 ? "block tab-content p-4 lg:ml-[10%] md:ml-[10%] sm:ml-[5%] ml-[10%]" : "hidden" }
+          >
+            <div
+              class="tab-pane  show active"
+              id="tabs-home"
+              role="tabpanel"
+              aria-labelledby="tabs-home-tab"
+            >
+              <div className="w-[100%] ">
+                <div className=" w-[100%] lg:ml-[1%] sm:ml-[10%] md:ml-[10%] ml-[5%]">
+                  <ul>
+                    {acompanments.map((item,index) => {
+                      return (
+                        <li
+                          onClick={() => {
+                            fireAnimation(item.url);
+                            setActiveItem(index)
+
+                          }}
+                          className={activeItem === index? "my-5 font-cormorant font-bold flex cursor-pointer hover:text-3xl transition-all ease-in duration-75 adafasfafaf p-2 bg-primary-color rounded-full text-white":"my-5 font-cormorant font-bold flex cursor-pointer hover:text-3xl transition-all ease-in duration-75"}
+                          key={item.name}
+                        >
+                          <img
+                            src={item.url}
+                            className="h-[40px] shadow-md  mr-3 w-[40px] rounded-full"
+                          />
+                          {item.name}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div
+              class="tab-pane "
+              id="tabs-profile"
+              role="tabpanel"
+              aria-labelledby="tabs-profile-tab"
+            ></div>
+          </motion.div> <motion.div
+            initial={{ x: -1500, scaleY: 0.2, scaleY: 0.2 }}
+            animate={{ x: 0, opacity:1, scaleY: 1, scaleX: 1}}
+            exit={{ y: 500, scaleY: 0, scaleX: 0 }}
+            transition={{
+              default: { duration: 1 },
+            }}
+            viewport={{ once: false }}
+            id="tabs-tabContent" className={toggletab === 3 ? "block tab-content p-4 lg:ml-[10%] md:ml-[10%] sm:ml-[5%] ml-[10%] visible " : "hidden" }
+          >
+            <div
+              class="tab-pane  show active"
+              id="tabs-home"
+              role="tabpanel"
+              aria-labelledby="tabs-home-tab"
+            >
+              <div className="w-[100%] ">
+                <div className=" w-[100%] lg:ml-[1%] sm:ml-[10%] md:ml-[10%] ml-[5%]">
+                  <ul>
+                    {cocktails.map((item,index) => {
+                      
+                      return (
+                        <li
+                          onClick={() => {
+                            fireAnimation(item.url);
+                            setActiveItem(index)
+                          }}
+                          className={activeItem === index? "my-5 font-cormorant font-bold flex cursor-pointer hover:text-3xl transition-all ease-in duration-75 adafasfafaf p-2 bg-primary-color rounded-full text-white":"my-5 font-cormorant font-bold flex cursor-pointer hover:text-3xl transition-all ease-in duration-75"}
+                          key={item.name}
+                        >
+                          <img
+                            src={item.url}
+                            className="h-[40px] transition-all ease-in hover:h-[50px]  delay-150 shadow-md  mr-3 w-[40px] rounded-full"
+                          />
+                          {item.name}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div
+              class="tab-pane "
+              id="tabs-profile"
+              role="tabpanel"
+              aria-labelledby="tabs-profile-tab"
+            ></div>
+          </motion.div>
         </div>
         <div className="w-[90%] col-span-2">
-          <AnimatePresence>
+       <AnimatePresence>
+       
+      
             {animate && (
               <motion.img
                 key={viewer}
@@ -210,9 +327,21 @@ const BarAndRestaurant = () => {
                   default: { duration: 0.2 },
                 }}
                 src={viewer}
-                className="h-[100%] m-10 w-[100%] object-cover rounded"
+                className="max-h-[80%] m-10 max-w-[80%] object-cover rounded"
               />
             )}
+             <div className={ !animate?"w-[100%] h-[100%] flex justify-center align-center text-center items-center visible" :"hidden"}>
+        <ThreeDots 
+height="80" 
+width="80" 
+radius="9"
+color="#4fa94d" 
+ariaLabel="three-dots-loading"
+wrapperStyle={{}}
+wrapperClassName=""
+visible={true}
+ />
+        </div>
           </AnimatePresence>
         </div>
         <div className="w-[100%] mb-14"></div>
